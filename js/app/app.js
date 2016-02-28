82cdb4f7a0a3e6d361a9179186ff93b17b966988
mahmoud-samy <msamy.earth@gmail.com>
2016-02-28 18:29:05 +0200

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -3,11 +3,11 @@ function ( THREE, camera, controls, stlModelLoader, light, material, renderer, s
   var app = {
     meshes: [],
     init: function () {
-      scene.add( new THREE.GridHelper( 500, 5 ) );
+      //scene.add( new THREE.GridHelper( 500, 5 ) );
   
       stlModelLoader.load( 'orders/62692.stl', function ( geometry ) {
         var mesh = new THREE.Mesh( geometry, material.phong );
-        mesh.position.set( 0, 0, 0 );
+        mesh.position.set( - 200, 0, 0 );
         mesh.rotation.set( - Math.PI/2 , 0, 0 );
         //mesh.scale.set( 0.005, 0.005, 0.005 );
         scene.add( mesh );
