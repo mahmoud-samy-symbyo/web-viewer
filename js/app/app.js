8da1d883abc1d5861454e507db1b2a53df97f38f
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-01 01:16:55 +0200

    code cleaning

--- js/app/app.js
+++ js/app/app.js
@@ -7,20 +7,11 @@ function ( THREE, camera, controls, stlModelLoader, light, material, renderer, s
   
       stlModelLoader.load( 'orders/62692.stl', function ( geometry ) {
         var mesh = new THREE.Mesh( geometry, material.phong );
-        mesh.position.set( - 200, 0, 0 );
-        mesh.rotation.set( - Math.PI/2 , 0, 0 );
-        //mesh.scale.set( 0.005, 0.005, 0.005 );
-        scene.add( mesh );
-        app.animate();
-      } );
-
-      stlModelLoader.load( 'orders/62692.stl', function ( geometry ) {
-        var mesh = new THREE.Mesh( geometry, material.phong );
         mesh.position.set( 0, 0, 0 );
         mesh.rotation.set( - Math.PI/2 , 0, 0 );
         //mesh.scale.set( 0.005, 0.005, 0.005 );
+        
         scene.add( mesh );
-        app.animate();
       } );
       //app.animate();
       //controls.addEventListener( 'change', app.animate );
