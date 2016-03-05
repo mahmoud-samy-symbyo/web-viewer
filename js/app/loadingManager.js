a93d1f40ac75e0b93aac980cfea6c7314d72001a
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-05 20:23:09 +0200

    Add: Loading progress [more work is required to enhance the UX]

--- js/app/loadingManager.js
+++ js/app/loadingManager.js
@@ -1,15 +1,35 @@
-define( ["three"], function ( THREE ) {
+define( ["three", "scene"], function ( THREE, scene ) {
+  var textMesh = undefined;
+  var fontLoader = new THREE.FontLoader();
+  fontLoader.load( 'js/fonts/helvetiker_bold.typeface.js', function ( font ) {
+    var materialFront = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var materialSide = new THREE.MeshBasicMaterial( { color: 0x000088 } );
+    var materialArray = [ materialFront, materialSide ];
+    var textGeom = new THREE.TextGeometry( "Loading ...", {
+      font: font, size: 10
+    });
+
+    var textMaterial = new THREE.MeshFaceMaterial(materialArray);
+    textMesh = new THREE.Mesh(textGeom, textMaterial );
+
+    textGeom.computeBoundingBox();
+    var textWidth = textGeom.boundingBox.max.x - textGeom.boundingBox.min.x;
+
+    textMesh.position.set( -0.5 * textWidth, 0, 0 );
+    //textMesh.rotation.x = -Math.PI / 4;
+    scene.add(textMesh);
+  });
+
   var loadingManager = new THREE.LoadingManager();
   loadingManager.onLoad = function ( ) {
-  	console.log( "onLoad" );
-    debugger;
+    if(textMesh) {
+      scene.remove(textMesh);
+    }
   };
   loadingManager.onProgress = function ( item, loaded, total ) {
-  	console.log( item, loaded, total );
   };
   loadingManager.onError = function ( ) {
-    console.log( "onError" );
-    debugger;
   };
+
   return loadingManager;
 } );
