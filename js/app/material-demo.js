8882ea92032531c50a18650ae2546f4216aa41fb
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-15 03:25:20 +0200

    Code cleaning.

--- js/app/material-demo.js
+++ js/app/material-demo.js
@@ -3,6 +3,8 @@ define( ["three", "shader!simple.vert", "shader!simple.frag", "texture"], functi
   // See `simple.frag` file, where `faceColor` is already defined to be white, and we are overriding it to red here
   simpleFrag.define( "faceColor", "vec3(1.0, 0, 0)" );
   return {
+    wire: new THREE.MeshBasicMaterial( { wireframe: true } ),
+    solid: new THREE.MeshLambertMaterial( {color: 0x00dcdc, shading: THREE.FlatShading }),
     bump: new THREE.MeshPhongMaterial( { bumpMap: texture.grass } ),
     grass: new THREE.MeshBasicMaterial( { map: texture.grass } ),
     shader: new THREE.ShaderMaterial( {
@@ -11,11 +13,6 @@ define( ["three", "shader!simple.vert", "shader!simple.frag", "texture"], functi
       },
       vertexShader: simpleVert.value,
       fragmentShader: simpleFrag.value
-    }),
-    solid: new THREE.MeshLambertMaterial( {
-      color: 0x00dcdc,
-      shading: THREE.FlatShading
-    }),
-    wire: new THREE.MeshBasicMaterial( { wireframe: true } )
+    })
   };
 } );
--- js/app/material.js
+++ js/app/material.js
@@ -1,9 +1,9 @@
 define( ["three"], function ( THREE ) {
 
   return {
-    phong: new THREE.MeshPhongMaterial( { color: 0xFFFFFF, specular: 0x999999, shininess: 200 } ),
+    wire: new THREE.MeshBasicMaterial( { wireframe: true } ),
     solid: new THREE.MeshLambertMaterial( { color: 0xdcdcdc }),
-    wire: new THREE.MeshBasicMaterial( { wireframe: true } )
+    phong: new THREE.MeshPhongMaterial( { color: 0xFFFFFF, specular: 0x999999, shininess: 200 } )
   };
-  
+
 } );
