945fb289cbe1a40f6ff41667f76c84556f10fcb3
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-15 06:04:34 +0200

    promises based stl loader (Breaking change)

--- js/app/stlModelLoader.js
+++ js/app/stlModelLoader.js
@@ -1,4 +1,17 @@
-define( ["three", "loadingManager", "STLLoader"], function ( THREE, loadingManager ) {
+define( ["rsvp", "three", "loadingManager", "STLLoader"], function ( RSVP, THREE, loadingManager ) {
 	var stlLoader = new THREE.STLLoader(loadingManager);
-	return stlLoader;
+
+	return function(url) {
+	  var promise = new RSVP.Promise(function(resolve, reject){
+				try {
+					stlLoader.load( url, function ( geometry ) {
+						resolve(geometry);
+					} );
+				} catch (e) {
+					reject(e);
+				}
+	  });
+	  return promise;
+	};
+
 } );
