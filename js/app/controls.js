1ba33d2a44c9246d6257715cb903bf94ce08a0fd
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-16 04:47:06 +0200

    Fix: deprecated code

--- js/app/controls.js
+++ js/app/controls.js
@@ -3,8 +3,8 @@ define( ["three", "camera", "container", "datgui", "renderer", "OrbitControls"],
 		controls = new THREE.OrbitControls( camera, renderer.domElement );
 		controls.minDistance = 50;
 		controls.maxDistance = 200;
-		controls.noPan = true;
-		controls.noZoom = false;
-		controls.noRotate = false;
+		controls.enablePan = false;
+		controls.enableZoom = true;
+		controls.enableRotate = true;
 		return controls;
 } );
