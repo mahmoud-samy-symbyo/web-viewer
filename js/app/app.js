e187bfb023daef0fd4dcfa8907e45e87866c9311
Mahmoud Samy <msamy.earth@gmail.com>
2016-03-16 02:30:44 +0200

    Fix: light problem

--- js/app/app.js
+++ js/app/app.js
@@ -1,19 +1,17 @@
-define(    ["appConfig","three","camera","meshHelpers","controls","stlReader","material","renderer","scene","statistics"],
-  function ( appConfig , THREE , camera , meshHelpers , controls , stlReader , material , renderer , scene , statistics ) {
+define(    ["appConfig","three","camera","meshHelpers","controls","stlReader","material","light","renderer","scene","statistics"],
+  function ( appConfig , THREE , camera , meshHelpers , controls , stlReader , material , light , renderer , scene , statistics ) {
     var app = {
         init: function () {
-          //scene.add( new THREE.GridHelper( 200, 10 ) );
-          //scene.add(new THREE.AxisHelper(1000));
-
           stlReader('orders/62692.stl').then(function ( geometry ) {
             var mesh = new THREE.Mesh( geometry, material.phong );
             meshHelpers.pointTo(mesh);
             scene.add( mesh );
-          }).catch(
-            function(e){ console.log(e);
+          }).catch( function(e){
+            console.log(e);
           });
+          //scene.add( new THREE.GridHelper( 200, 10 ) );
+          //scene.add(new THREE.AxisHelper(1000));
         },
-
 
         render: function () {
           controls.update();
